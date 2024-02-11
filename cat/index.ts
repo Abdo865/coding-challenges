743713fdf601205165bec3165b51ee851842194e
Abdo Nasser <abdonasser865@gmail.com>
2024-02-11 23:33:07 +0200

    module(cat: step 4): Adding line numbers is supported

--- cat/index.ts
+++ cat/index.ts
@@ -2,17 +2,48 @@ import fs from "fs";
 
 const args = process.argv.slice(2);
 
-for (let i = 0; i < args.length; i++) {
-  readAllFile(args[i]);
+let lineNum = false,
+  data = "",
+  files: string[] = [];
+
+handleInput(args);
+
+function handleInput(args: string[]) {
+  if (args[0] === "-n") {
+    lineNum = true;
+    args.shift();
+  }
+
+  if (!process.stdin.isTTY) {
+    data = readFromStdin();
+  } else {
+    files = args;
+    for (let i = 0; i < files.length; i++) data += readAllFile(files[i]);
+  }
+  if (lineNum) data = addLineNum(data);
+  console.log(data);
 }
 
-function readAllFile(file: string) {
+function readAllFile(file: string): string {
   try {
-    const data = fs.readFileSync(file, "utf-8");
-    console.log(data);
+    const fileData = fs.readFileSync(file, "utf-8");
+    return fileData;
   } catch (error: any) {
-    if (error["errno"] === -2) {
-      console.log(`cat: ${file}: No such file or directory`);
-    }
+    if (error["errno"] === -2)
+      throw new Error(`cat: ${file}: No such file or directory`);
+    else throw new Error(`cat: ${file}: ${error}`);
   }
+}
+
+function readFromStdin(): string {
+  const inputData = fs.readFileSync(process.stdin.fd, "utf-8");
+  return inputData;
+}
+
+function addLineNum(data: string): string {
+  let lines = data.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    lines[i] = `${(i + 1).toString().padEnd(6, " ")}${lines[i]}`;
+  }
+  return lines.join("\n");
 }
